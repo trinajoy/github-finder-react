d757f55914c64f844afa0171605360854167de85
Katrina H <katrina.hitchcock@gmail.com>
2020-11-06 13:15:06 +0100

    add search functionality

--- src/App.js
+++ src/App.js
@@ -1,36 +1,48 @@
-import "./App.css"
-import React, {Component} from "react"
-import Navbar from "./components/layout/Navbar"
-import Users from "./components/users/Users"
-import axios from "axios"
-import "./App.css"
+import "./App.css";
+import React, {Component} from "react";
+import Navbar from "./components/layout/Navbar";
+import Search from "./components/users/Search";
+import Users from "./components/users/Users";
+import axios from "axios";
+import "./App.css";
 
 class App extends React.Component {
   state = {
     users: [],
     loading: false,
-  }
+  };
 
-  async componentDidMount() {
-    console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET)
+  // async componentDidMount() {
+  //   console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET);
 
-    this.setState({loading: true})
-    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
-    console.log(res.data)
+  //   this.setState({loading: true});
+  //   const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+  //   console.log(res.data);
 
-    this.setState({users: res.data, loading: false})
-  }
+  //   this.setState({users: res.data.items, loading: false});
+  // }
+
+  // search Github users
+  searchUsers = async text => {
+    this.setState({loading: true});
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({users: res.data.items, loading: false});
+    console.log(text);
+  };
 
   render() {
     return (
       <div className="App">
         <Navbar />
+
         <div className="container">
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
---
+++ src/components/users/Search.js
@@ -0,0 +1,33 @@
+import React, {Component} from "react";
+import PropTypes from "prop-types";
+
+export class Search extends Component {
+  state = {
+    text: "",
+  };
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+  };
+
+  onChange = e => this.setState({[e.target.name]: e.target.value});
+
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.searchUsers(this.state.text);
+    this.setState({text: ""}); // clears form
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onSubmit} className="form">
+          <input type="text" name="text" placeholder="search users" value={this.state.text} onChange={this.onChange} />
+          <input type="submit" name="search" className="btn btn-dark btn-block" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Search;
